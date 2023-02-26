eb85e682390878dc98a978a7c219ff2b5ba91756
Rajkeshwar Prasad <rajkeshwar.pd@gmail.com>
2023-02-26 08:08:08 +0100

    Initial commit for laptop-service-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import { Button } from 'primereact/button';
+import dayjs from 'dayjs';
+import { startCase } from 'lodash';
 
 function App() {
+  const today = dayjs().format('DD/MM/YYYY h:mm')
+  const label = startCase('last_modified_date');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="md:container md:mx-auto">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello world! {today}
+      </h1>
+      <p>{label}</p>
+      <div className="grid grid-cols-7 gap-4">
+        <Button icon="pi pi-check" aria-label="Filter" />
+        <Button icon="pi pi-bookmark" severity="secondary" aria-label="Bookmark" />
+        <Button icon="pi pi-search" severity="success" aria-label="Search" />
+        <Button icon="pi pi-user" severity="info" aria-label="User" />
+        <Button icon="pi pi-bell" severity="warning" aria-label="Notification" />
+        <Button icon="pi pi-heart" severity="help" aria-label="Favorite" />
+        <Button icon="pi pi-times" severity="danger" aria-label="Cancel" />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";  
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
